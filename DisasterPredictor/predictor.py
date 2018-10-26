4a975c6624cdc2656dd0d2e6e99c9d3e43500c3f
Var-ji <varunparthasarathy7@gmail.com>
2018-10-26 11:19:17 +0530

    Improvement : Modify code to accept multiple entries at once and return               disaster with highest frequency and probability

--- DisasterPredictor/predictor.py
+++ DisasterPredictor/predictor.py
@@ -1,22 +1,28 @@
 from keras.models import load_model
 import numpy as np
+from collections import Counter
 
 
-def predict(lat, long, hour, minute, sec):
-    data = [[lat, long, hour, minute, sec]]
+def predict(data):
+    results = list()
     data = np.vstack(data)
     model = load_model('disaster_model.h5')
-    predictions = list(model.predict(data)[0])
-    probability = max(predictions)
-    disaster = predictions.index(probability)
-    if disaster == 0:
-        return 'typhoon'
-    elif disaster == 1:
-        return 'earthquake'
-    elif disaster == 2:
-        return 'winter_storm'
-    elif disaster == 3:
-        return 'thunderstorm'
-    elif disaster == 4:
-        return 'wildfire'
+    predictions = model.predict(data)
+    for prediction in predictions:
+        prediction = list(prediction)
+        probability = max(prediction)
+        disaster = prediction.index(probability)
+        if disaster == 0:
+            results.append('typhoon')
+        elif disaster == 1:
+            results.append('earthquake')
+        elif disaster == 2:
+            results.append('winter_storm')
+        elif disaster == 3:
+            results.append('thunderstorm')
+        elif disaster == 4:
+            results.append('wildfire')
+
+    dis = Counter(results)
+    return dis.most_common(1)[0][0]
 
