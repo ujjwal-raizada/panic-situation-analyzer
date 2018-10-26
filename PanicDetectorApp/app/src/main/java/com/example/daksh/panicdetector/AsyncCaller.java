dde1da3f089edc29b315e322d05b72cb7e2c931e
Ujjwal Raizada <ujjwalraizada@gmail.com>
2018-10-27 02:59:53 +0530

    Android: Add android app for notification

---
+++ PanicDetectorApp/app/src/main/java/com/example/daksh/panicdetector/AsyncCaller.java
@@ -0,0 +1,35 @@
+package com.example.daksh.panicdetector;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AsyncCaller extends AsyncTask<Void, Void, Void> {
+
+    private static final String TAG = "AsyncCaller";
+
+    @Override
+    protected Void doInBackground(Void... voids) {
+        final Map<String, String> json = formatDataAsJSON();
+        Log.d(TAG, "doInBackground: " + json);
+        ExampleJobService.getServerResponse(json);
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+    }
+
+    private Map<String, String> formatDataAsJSON(){
+
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("username", ExampleJobService.username);
+        params.put("lng", ExampleJobService.longitude);
+        params.put("lat", ExampleJobService.latitude);
+        return params;
+
+    }
+}
---
+++ PanicDetectorApp/app/src/main/java/com/example/daksh/panicdetector/MainActivity.java
@@ -0,0 +1,122 @@
+package com.example.daksh.panicdetector;
+
+import android.Manifest;
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public EditText editText;
+
+    private static final String TAG = "MainActivity";
+    private static final int REQUEST_FINE_LOCATION = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = (EditText)findViewById(R.id.edittext);
+
+
+        getLocationPermission();
+        //Check permission runtime
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_FINE_LOCATION);
+        }
+
+    }
+
+    /*public void scheduleJob(View view){
+        ComponentName componentName = new ComponentName(this, ExampleJobService.class);
+        JobInfo info = new JobInfo.Builder(123, componentName)
+                .setPersisted(true)
+                .setPeriodic(15*60*1000)
+                .build();
+
+        JobScheduler scheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
+        int resultCode = scheduler.schedule(info);
+        if(resultCode == JobScheduler.RESULT_SUCCESS){
+            Log.d(TAG, "scheduleJob: Job Scheduled");
+        }
+        else{
+            Log.d(TAG, "scheduleJob: Job Scheduling Failed");
+        }
+
+    }
+
+    public void cancelJob(View view){
+        JobScheduler scheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
+        scheduler.cancel(123);
+        Log.d(TAG, "cancelJob: Job Cancelled");
+
+    }*/
+
+    private void getLocationPermission(){
+        Log.d(TAG, "getLocationPermission: getting location permissions");
+        String[] permissions = {Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION};
+
+        if(ContextCompat.checkSelfPermission(this.getApplicationContext(),Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(this.getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                Log.d(TAG, "getLocationPermission: Permission Granted");
+            } else {
+                ActivityCompat.requestPermissions(this, permissions, REQUEST_FINE_LOCATION);
+            }
+
+        }else {
+            ActivityCompat.requestPermissions(this, permissions, REQUEST_FINE_LOCATION);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case REQUEST_FINE_LOCATION: {
+                if (grantResults.length > 0) {
+                    if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                        Log.d(TAG, "onRequestPermissionsResult: Permission Granted");
+
+                    } else if (grantResults[0] == PackageManager.PERMISSION_DENIED) {
+                        ActivityCompat.requestPermissions(this,
+                                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                REQUEST_FINE_LOCATION);
+
+                    }
+                }
+            }
+        }
+    }
+
+    public void setUsername(View view){
+        ExampleJobService.username = editText.getText().toString();
+        ComponentName componentName = new ComponentName(this, ExampleJobService.class);
+        JobInfo info = new JobInfo.Builder(123, componentName)
+                .setPersisted(true)
+                .setPeriodic(15*60*1000)
+                .build();
+
+        JobScheduler scheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
+        assert scheduler != null;
+        int resultCode = scheduler.schedule(info);
+        if(resultCode == JobScheduler.RESULT_SUCCESS){
+            Log.d(TAG, "scheduleJob: Job Scheduled");
+            Toast.makeText(this,"Detection started successfully",Toast.LENGTH_LONG).show();
+        }
+        else{
+            Log.d(TAG, "scheduleJob: Job Scheduling Failed");
+        }
+        editText.setText("Enter Username");
+    }
+}
